4f0060e5b62635a06cd1c60e096d6b04a5c35d76
Enrique Berrueta <eabz@polispay.org>
2021-02-21 18:34:23 -0600

    Upload Plutus and Senate interfaces, add Senate test and organize folders

--- contracts/senate/Senate.sol
+++ contracts/senate/Senate.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Senate  {
     
