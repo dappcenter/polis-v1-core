8476b5010c7f862a3a6666d222a5c1144bbdc368
Aether-eum <aether@polispay.org>
2021-03-21 10:12:46 -0600

    Contract Guard optimization

--- contracts/plutus/ContractGuard.sol
+++ contracts/plutus/ContractGuard.sol
@@ -6,19 +6,12 @@ pragma solidity ^0.7.0;
 contract ContractGuard {
     mapping(uint256 => mapping(address => bool)) private _status;
 
-    function checkSameOriginReentranted() internal view returns (bool) {
-        return _status[block.number][tx.origin];
-    }
-
     function checkSameSenderReentranted() internal view returns (bool) {
         return _status[block.number][msg.sender];
     }
 
     modifier onlyOneBlock() {
-        require(
-            !checkSameOriginReentranted(),
-            'ContractGuard: one block, one function'
-        );
+
         require(
             !checkSameSenderReentranted(),
             'ContractGuard: one block, one function'
@@ -26,7 +19,6 @@ contract ContractGuard {
 
         _;
 
-        _status[block.number][tx.origin] = true;
         _status[block.number][msg.sender] = true;
     }
 }
